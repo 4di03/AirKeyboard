db1291e4ee5852f233c6018b10644ecf81c23409
Adithya <adithyapalle12@gmail.com>
2023-11-22 01:47:13 -0500

    Webcam fixes

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <torch/torch.h>
+//#include <torch/torch.h>
+#include <ctime>
 
 
 int main() {
     // Open a connection to the webcam (usually 0 for the default webcam)
     cv::VideoCapture cap(0);
 
-    torch::Tensor tensor = torch::rand({2, 3});
-    std::cout << tensor << std::endl;
+    std::cout << "Expected FPS: " << cap.get(cv::CAP_PROP_FPS) << std::endl;
+    int ct = 0;
+
+
+    // torch::Tensor tensor = torch::rand({2, 3});
+    // std::cout << tensor << std::endl;
 
     // Check if the webcam opened successfully
     if (!cap.isOpened()) {
@@ -20,8 +25,13 @@ int main() {
     // Create a window to display the webcam feed
     cv::namedWindow("Webcam Feed", cv::WINDOW_NORMAL);
 
+    // get start time 
+    std::time_t result = std::time(nullptr);
+
+
     // Main loop
     while (true) {
+        ct++;
         // Capture a frame from the webcam
         cv::Mat frame;
         cap >> frame;
@@ -32,8 +42,18 @@ int main() {
             break;
         }
 
+        std::time_t cur_time = std::time(nullptr);
+    
+
+        float time_diff = cur_time - result;
+        std::cout << "time diff: " << time_diff << std::endl;
+        float avg_fps = ct/ time_diff;
+
+        
         // Display the frame in the window
         cv::imshow("Webcam Feed", frame);
+
+        std::cout << "Current FPS: " << avg_fps << std::endl;
 
         // Break the loop if the user presses the 'ESC' key
         if (cv::waitKey(1) == 27) {
